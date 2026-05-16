defbe273843f59ba6789426fc083c24286335904
roshdyosf <yosef2236303@gmail.com>
2026-05-17 02:41:14 +0300

    Implement initial ProductRepository class with placeholder methods

---
+++ src/domain/repositories/productRepository.js
@@ -0,0 +1,12 @@
+class ProductRepository {
+    async save(product) {
+        throw new Error('Method not implemented');
+    }
+    async findById(id) {
+        throw new Error('Method not implemented');
+    }
+    async findAll() {
+        throw new Error('Method not implemented');
+    }
+
+}
