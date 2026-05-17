5047a7b48c2fdceb477d92bee5b2d9ec414a09d2
roshdyosf <yosef2236303@gmail.com>
2026-05-17 03:47:46 +0300

    - Introduced `MongoProductRepository` for handling product data with MongoDB. - Implemented methods for saving, finding by ID, and finding all products with optional filters.

---
+++ src/infrastructure/repositories/mongoProductRepository.js
@@ -0,0 +1,59 @@
+import ProductRepository from "../../domain/repositories/productRepository.js";
+import Product from "../../domain/entities/product.js";
+class MongoProductRepository extends ProductRepository {
+    constructor(productModel) {
+        this.productModel = productModel;
+    }
+
+    async save(productEntity) {
+        const productDoc = new this.productModel({
+            _id: productEntity.id,
+            name: productEntity.name,
+            price: productEntity.price,
+            provider: productEntity.provider,
+            description: productEntity.description,
+            quantity: productEntity.quantity
+        });
+
+        return await productDoc.save();
+    }
+
+
+    async findById(id) {
+        const product = await this.productModel.findById(id);
+        if (!product) {
+            throw new Error('Product not found');
+        }
+        return new Product(
+            product._id,
+            product.name,
+            product.price,
+            product.provider,
+            product.description,
+            product.quantity);
+
+    }
+
+    async findAll(filters = {}) {
+
+        if (filters.name) {
+            filters.name = { $regex: filters.name, $options: 'i' };
+        }
+        if (filters.provider) {
+            filters.provider = { $regex: filters.provider, $options: 'i' };
+        }
+
+        const products = await this.productModel.find(filters);
+
+        return products.map(product => new Product(
+            product._id,
+            product.name,
+            product.price,
+            product.provider,
+            product.description,
+            product.quantity
+        ));
+
+    }
+}
+export default MongoProductRepository;
