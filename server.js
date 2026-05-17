4df5b19a9477481e5444c35ce1f4bb497488b07a
roshdyosf <yosef2236303@gmail.com>
2026-05-17 04:59:49 +0300

    Add server and app setup with middleware for logging, rate limiting, and data sanitization

---
+++ server.js
@@ -0,0 +1,37 @@
+import app from './src/infrastructure/webserver/app.js';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 3000;
+
+
+let server;
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    family: 4, // Use IPv4, comment this if you need IPv6
+}).then(() => {
+    console.log('Connected to MongoDB');
+
+    server = app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+
+}).catch(err => {
+    console.log('DB Connection Error: ', err.message);
+});
+
+
+
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+});
---
+++ src/infrastructure/webserver/app.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
+
+import AppError from './utils/AppError.js';
+import { globalErrorHandler } from './middlewares/errorMiddleware.js';
+
+// un finshed file, just to show the global error handler and middlewares in action.
+// The routes and controllers are not implemented yet.
+
+const app = express();
+// 1. Global Middlewares
+
+// 1.1 Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// 1.2 Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000, // 1 hour
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// 1.3 Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// 1.4 Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// 1.5 Prevent parameter pollution
+app.use(hpp());
+
+// 2. Routes
+// --- IGNORE ---
+
+
+
+
+
+
+// 3. Handle unhandled routes
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// 4. Global Error Handler
+app.use(globalErrorHandler);
+
+
