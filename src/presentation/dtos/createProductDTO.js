66a3670900b168f1005bc1d8f8e777dafc64ebcd
roshdyosf <yosef2236303@gmail.com>
2026-05-17 05:56:38 +0300

    Add CreateProductDTO and ProductResponseDTO for product data handling

---
+++ src/presentation/dtos/createProductDTO.js
@@ -0,0 +1,14 @@
+import { object } from "joi";
+
+class CreateProductDTO {
+    constructor(name, price, description, provider, quantity) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.provider = provider;
+        this.quantity = quantity;
+        object.freeze(this);
+    }
+}
+
+export default CreateProductDTO;
---
+++ src/presentation/dtos/productResponseDTO.js
@@ -0,0 +1,13 @@
+class ProductResponseDTO {
+    constructor(productEntity) {
+        this.id = productEntity.id;
+        this.name = productEntity.name;
+        this.price = productEntity.price;
+        this.provider = productEntity.provider;
+        this.description = productEntity.description || "No description provided";
+        this.status = productEntity.quantity > 0 ? "In Stock" : "Out of Stock";
+        object.freeze(this);
+    }
+}
+
+export default ProductResponseDTO;
